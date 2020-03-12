e1d84247843a602b86de5bf86f45094eb31f151e
Long Loi <long@vault.co>
2020-03-12 08:10:37 -0500

    [GENIUS-8013] Finicity: Capture and Store Statements

---
+++ lib/finicity/resources/statement.rb
@@ -0,0 +1,11 @@
+module Finicity
+  module Resources
+    class Statement < Base
+      # Gets the latest statement
+      # https://community.finicity.com/s/article/Statement-Aggregation#get_customer_account_statement
+      def get_latest(account_id)
+        request(:get, "/aggregation/v1/customers/#{customer_id}/accounts/#{account_id}/statement")
+      end
+    end
+  end
+end
