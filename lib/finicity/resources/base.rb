ff3b9da25cef71e36e168191aeff39c950fd1920
Long Loi <long@vault.co>
2020-03-12 08:10:37 -0500

    [GENIUS-8013] Finicity: Capture and Store Statements

--- lib/finicity/resources/base.rb
+++ lib/finicity/resources/base.rb
@@ -13,12 +13,20 @@ module Finicity
         ::Finicity::Fetchers::API.request(*args)
       end
 
+      def self.request_download(*args)
+        ::Finicity::Fetchers::API.request_download(*args)
+      end
+
       protected
 
       # TODO: can be combined with .request above
       # Proxy used by the resource classes
       def request(*args)
         self.class.request(*args)
+      end
+
+      def request_download(*args)
+        self.class.request_download(*args)
       end
     end
   end
--- lib/finicity/resources/statement.rb
+++ lib/finicity/resources/statement.rb
@@ -4,7 +4,7 @@ module Finicity
       # Gets the latest statement
       # https://community.finicity.com/s/article/Statement-Aggregation#get_customer_account_statement
       def get_latest(account_id)
-        request(:get, "/aggregation/v1/customers/#{customer_id}/accounts/#{account_id}/statement")
+        request_download("https://api.finicity.com/aggregation/v1/customers/#{customer_id}/accounts/#{account_id}/statement")
       end
     end
   end
